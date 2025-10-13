03e661c2bbf79d5933783fa367d5626646ea8156
hjh44096 <hanjuheon126@gmail.com>
2025-10-13 17:37:21 +0900

    loops/main.go v0.1 if

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n')
+	//i, _ := r.ReadString('\n') //ignore error
+	i, err := r.ReadString('\n') //ignore error
+	//fmt.Println(err)
+	if err != nil {
+		log.Fatal(err) // report the error exit the progrem
+	}
 	fmt.Println(i)
 }
